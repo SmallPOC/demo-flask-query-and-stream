51e75b38c31e4d4988a116cb9d8c16d9289a95b5
nirvikalpa <experimentalhypothesis@gmail.com>
2020-05-09 19:18:02 +0200

    slight change in naming

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template, request
 from time import time
-
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///playlist.db'
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-class MemberDB(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    added = db.Column(db.DateTime)
-
 class Songs(db.Model):
+    """ data model for song """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     artist = db.Column(db.String)
@@ -25,27 +18,18 @@ class Songs(db.Model):
     path = db.Column(db.String)
 
     def __repr__(self):
-        return f"{self.title} - {self.artist} - {self.album}"
+        title = " ".join(self.title.split()[:-1])
+        return f"{title} - {self.artist} - {self.album}"
 
 db.create_all()
-
-def get_query_result(query:str) -> list:
-    q = Songs.query.filter_by(artist=query).all()
-    # result = []
-    # for i in q:
-    #     title = " ".join(i.title.split()[:-1])
-    #     print(title, type(title))
-    #     ret = f"{i.artist}  -  {i.album}  -  {title} - {i.id} - {i.path}"
-    #     result.append(ret)
-    return result      
 
 
 @app.route("/", methods=["GET", "POST"])
 def search():
+    """ route for seeing results of a search """
     if request.method == 'POST':
         query = request.form.get("artist")
         qry_result = Songs.query.filter_by(artist=query.title()).all()
-        # print(qry_result
         if qry_result:
             return render_template("search.html", data=qry_result)
         else:
@@ -54,18 +38,11 @@ def search():
     return render_template("search.html")
 
 
-
 @app.route("/play/<int:id>")
 def play(id:int):
+    """ route for playing selected song """
     song = Songs.query.get_or_404(id)
-    return render_template("play.html", path=song.path)
-
-
-# @app.route("/post/<int:post_id>")
-# def post(post_id:int):
-#     """ route for seeing a single post """
-#     post = Post.query.get_or_404(post_id)
-#     return render_template("post.html", title=post.title, post=post)
+    return render_template("play.html", path=song.path, title=" ".join(song.title.split()[:-1]), artist=song.artist, album=song.album )
 
 
 if __name__=="__main__":
