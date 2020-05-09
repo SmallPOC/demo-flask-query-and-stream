1535bf154a98093e1d2160bdd8d488746e5e8b36
nirvikalpa <experimentalhypothesis@gmail.com>
2020-05-09 18:59:47 +0200

    on click it redirects to a proper link

--- app.py
+++ app.py
@@ -24,16 +24,19 @@ class Songs(db.Model):
     album = db.Column(db.String)
     path = db.Column(db.String)
 
+    def __repr__(self):
+        return f"{self.title} - {self.artist} - {self.album}"
+
 db.create_all()
 
 def get_query_result(query:str) -> list:
     q = Songs.query.filter_by(artist=query).all()
-    result = []
-    for i in q:
-        title = " ".join(i.title.split()[:-1])
-        print(title, type(title))
-        ret = f"{i.artist}  -  {i.album}  -  {title}"
-        result.append(ret)
+    # result = []
+    # for i in q:
+    #     title = " ".join(i.title.split()[:-1])
+    #     print(title, type(title))
+    #     ret = f"{i.artist}  -  {i.album}  -  {title} - {i.id} - {i.path}"
+    #     result.append(ret)
     return result      
 
 
@@ -41,22 +44,28 @@ def get_query_result(query:str) -> list:
 def search():
     if request.method == 'POST':
         query = request.form.get("artist")
-        titlecased_qry = query.title()
-        qry_result = get_query_result(titlecased_qry)
-        print(qry_result)
+        qry_result = Songs.query.filter_by(artist=query.title()).all()
+        # print(qry_result
         if qry_result:
             return render_template("search.html", data=qry_result)
         else:
-            msg = f"sorry no resuf found for '{query}'"
+            msg = f"sorry no result found for '{query}'"
             return render_template("search.html", msg=msg)
     return render_template("search.html")
 
 
 
-@app.route("/play/<artist>/<album>/<title>")
-def play_song(artist, album, title):
-    return f"playing.. {artist} - {album} - {title}"
+@app.route("/play/<int:id>")
+def play(id:int):
+    song = Songs.query.get_or_404(id)
+    return render_template("play.html", path=song.path)
 
+
+# @app.route("/post/<int:post_id>")
+# def post(post_id:int):
+#     """ route for seeing a single post """
+#     post = Post.query.get_or_404(post_id)
+#     return render_template("post.html", title=post.title, post=post)
 
 
 if __name__=="__main__":
