a2d6fe8bee06ec4b205dfbb04cbe72ee10568558
nirvikalpa <experimentalhypothesis@gmail.com>
2020-05-10 23:50:24 +0200

    streaming queried song

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+""" This is a simple demo showing how to query an audio file from a database and stream it online. It is one file app for simple demonstration of the concept (querying and streaming the chosen file) """ 
+
+from flask import Flask, Response, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from time import time
 import os
@@ -8,7 +10,6 @@ db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
 app.config["SQLALCHEMY_ECHO"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 class Songs(db.Model):
     """ data model for song """
@@ -41,37 +42,38 @@ def search():
 
 @app.route("/play/<int:id>")
 def play(id:int):
-    """ route for playing selected song """
+    """ route for playing a song selected from database """
     song = Songs.query.get_or_404(id)
-    return render_template("play.html", path=song.path, title=song.title, artist=song.artist, album=song.album )
+    def gen():
+        with open(song.path, "rb") as f:
+            data = f.read(1024)
+            while data:
+                yield data
+                data = f.read(1024)
+    return Response(gen(), mimetype="audio/mp3")
 
 
-# def make_playlist(path:str):
-#     with open("winplaylist.m3u", "w") as f:
-#         for path, dirs, folders in os.walk(path):
-#             for file in folders:
-#                 if file.endswith(".mp3"):
-#                     print(os.path.join(path, file), file=f)
+def fill_db(root:str):
+    """ function that fills the database with song audio files info """
+    for path, dirs, folder in os.walk(root):
+        for file in folder:
+            if file.endswith(".mp3"):
+                print(file)
+                artist, album, title = file.strip(".mp3").split(" -- ")
+                artist = " ".join(artist.split(" ")[1:])
+                song = Songs(title=title, album=album, artist=artist, path=os.path.join(root, file))
+                db.session.add(song)
+                db.session.commit()
 
 
-def fill_db(path:str):
-    with open("winplaylist.m3u", "w") as f:
-        for path, dirs, folder in os.walk(root):
-            for file in folder:
-                if file.endswith(".mp3"):
-                    print(file)
-                    artist, album, title = file.strip(".mp3").split(" -- ")
-                    artist = " ".join(artist.split(" ")[1:])
-                    song = Songs(title=title, album=album, artist=artist, path=os.path.join(root, file))
-                    db.session.add(song)
-                    db.session.commit()
-
-
-if __name__=="__main__":
-    Songs.query.delete()
+def main():
+    Songs.query.delete() # deleting DB each time it runs.. just for debug puropose to work with clean..
     root = r"audio"
-    # make_playlist(root)
     fill_db(root)
     app.run(debug=True, host="localhost")
+   
+
+if __name__=="__main__":
+    main()
    
 
